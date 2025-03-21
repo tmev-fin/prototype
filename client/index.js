f03dce4666e7dd82f0ab5f27f87d6b20018ec80f
Ted <ted@Teds-MacBook-Pro.local>
2025-03-21 15:56:23 -0400

    refactored client

--- client/index.js
+++ client/index.js
@@ -1,22 +1,12 @@
-import * as FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
-
-const fpPromise = FingerprintJS.load({ apiKey: process.env.PUBLIC_API_KEY });
-
 (async () => {
-  const fp = await fpPromise;
-  const result = await fp.get({ extendedResult: true });
-  const visitorId = result.visitorId;
-  
-  document.getElementById('visitor-id').textContent = visitorId;
-  console.log(JSON.stringify(result, null, 2));
+    try {
+        const fp = await (await import('https://fpjscdn.net/v3/KDOFEu4EComVHSKj6vyu')).load();
+        const result = await fp.get();
+        const visitorId = result.visitorId;
 
-  // also send result to server so it can access the requestId
-  // fetch('http://localhost:3001', {
-  //  method: 'POST',
-  //  headers: {
-  //    'Content-Type': 'application/json'
-  //  },
-  //  body: JSON.stringify(result)
-  //});
-  // retrieve POST response from server
-})()
+        document.getElementById('visitor-id').textContent = visitorId;
+        console.log(JSON.stringify(result, null, 2));
+    } catch (error) {
+        console.error(`Error loading FingerprintJS: ${error}`);
+    }
+})();
