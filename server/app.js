492476040eedd83bd9593c0ed98474d6de988336
Ted <ted@Teds-MacBook-Pro.local>
2025-03-25 16:42:09 -0400

    updated cors options

--- server/app.js
+++ server/app.js
@@ -1,27 +1,36 @@
-import { FingerprintJsServerApiClient, Region } from '@fingerprintjs/fingerprintjs-pro-server-api'
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import {
+  FingerprintJsServerApiClient,
+  Region,
+} from "@fingerprintjs/fingerprintjs-pro-server-api";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(cors());
 dotenv.config();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://prototype-client.onrender.com",
+    methods: ["POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 const client = new FingerprintJsServerApiClient({
   apiKey: process.env.PRIVATE_API_KEY,
   region: Region.Global,
-})
+});
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   const requestId = req.body.requestId;
 
-  client.getEvent(requestId).then((data) => { 
+  client.getEvent(requestId).then((data) => {
     const event = JSON.stringify(data, null, 2);
     res.json(event);
-  })
+  });
 });
 
 app.listen(port, () => {
