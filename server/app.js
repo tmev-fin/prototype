1f8d63e4487e0bcd0fa5989715d719f4ca09d6d0
Ted <ted@Teds-MacBook-Pro.local>
2025-03-25 17:09:51 -0400

    allowing GET requests in cors options

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://prototype-client.onrender.com",
-    methods: ["POST", "OPTIONS"],
+    methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
   })
 );
