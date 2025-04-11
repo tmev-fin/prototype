7bd041739fabf77da7d8a34a1e14dd6190804f8a
Ted <ted@Teds-MacBook-Pro.local>
2025-04-11 16:02:19 -0400

    initial pt2 done

--- client/index.js
+++ client/index.js
@@ -1,39 +1,189 @@
 (async () => {
-  try {
-    const FingerprintJS = await import("https://fp.projectshowcase.dev/web/v3/KDOFEu4EComVHSKj6vyu");
-    const fp = await FingerprintJS.load({
-      endpoint: [
-        "https://fp.projectshowcase.dev",
-        FingerprintJS.defaultEndpoint,
-      ],
-    });
+  // ----- FingerprintJS setup -----
+  const FingerprintJS = await import('https://fp.projectshowcase.dev/web/v3/KDOFEu4EComVHSKj6vyu');
+  const fp = await FingerprintJS.load({
+    endpoint: ['https://fp.projectshowcase.dev', FingerprintJS.defaultEndpoint],
+  });
 
-    const results = await fp.get({ extendedResult: true });
-    const result = JSON.stringify(results, null, 2);
-    const visitorId = results.visitorId;
-    const requestId = results.requestId;
+  // ----- Web Awesome setup -----
+  const sectionAnchors = document.querySelectorAll("[slot*='navigation'] a[href*='#']");
+  sectionAnchors.forEach((sectionAnchor) => sectionAnchor.setAttribute('data-drawer', 'close'));
 
-    document.getElementById("visitor-id").textContent = visitorId;
-    document.getElementById("request-id").textContent = requestId;
-    console.log("Full JSON result from the Client:\n\n", result);
+  // ----- Dialog functionality -----
+  const signInDialog = document.querySelector('.dialog-signin');
+  const signInBtn = signInDialog.nextElementSibling;
 
-    try {
-      const response = await fetch("https://prototype-backend-hw7a.onrender.com/", {
-      // const response = await fetch("http://localhost:3001/", { // for local use
-        method: "POST",
+  signInBtn.addEventListener('click', () => {
+    signInDialog.open = true;
+  });
+
+  const registerDialog = document.querySelector('.dialog-register');
+  const registerBtn = registerDialog.nextElementSibling;
+
+  registerBtn.addEventListener('click', () => {
+    registerDialog.open = true;
+  });
+
+  const mfaDialog = document.querySelector('.dialog-mfa');
+
+  // ----- Dialog switching -----
+  const registerLink = document.querySelector('.registerLink');
+  const signinLink = document.querySelector('.signinLink');
+
+  registerLink.addEventListener('click', () => {
+    registerDialog.open = true;
+    signInDialog.open = false;
+  });
+
+  signinLink.addEventListener('click', () => {
+    registerDialog.open = false;
+    signInDialog.open = true;
+  });
+
+  // ----- Form submission -----
+  const signinForm = document.querySelector('.signin-form');
+  const registerForm = document.querySelector('.register-form');
+  const mfaForm = document.querySelector('.mfa-form');
+
+  // Wait for controls to be defined before attaching form listeners
+  await Promise.all([
+    customElements.whenDefined('wa-button'),
+    customElements.whenDefined('wa-input'),
+  ]).then(() => {
+    signinForm.addEventListener('submit', async (event) => {
+      // Signin form submission
+      event.preventDefault();
+      signInDialog.open = false;
+
+      const email = signinForm.querySelector("wa-input[label='Email']").value;
+      const password = signinForm.querySelector("wa-input[label='Password']").value;
+      const results = await fp.get({ extendedResult: true });
+      const visitorId = results.visitorId;
+
+      // prod: "https://prototype-backend-hw7a.onrender.com/signin"
+      // dev: "http://localhost:3001/signin"
+      const response = await fetch('http://localhost:3001/signin', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: result,
+        body: JSON.stringify({ email, password, visitorId }),
       });
 
-      const data = await response.json();
-      console.log("Response from server:\n\n", data);
-      document.getElementById("data-id").textContent = data;
-    } catch (fetchError) {
-      console.error("Error performing Fetch Request:", fetchError);
-    }
-  } catch (error) {
-    console.error(`Error loading FingerprintJS: ${error}`);
-  }
+      const result = await response.json();
+
+      if (!response.ok) {
+        // Check if MFA is needed
+        if (result.message === 'MFA needed before completing signin') {
+          mfaDialog.open = true;
+
+          mfaForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            mfaDialog.open = false;
+
+            const mfaCode = mfaForm.querySelector(
+              "wa-input[label='Multi-Factor Authentication Code']"
+            ).value;
+
+            // prod: "https://prototype-backend-hw7a.onrender.com/verify"
+            // dev: "http://localhost:3001/verify"
+            const response = await fetch('http://localhost:3001/verify', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ email, visitorId, mfaCode }),
+            });
+
+            const result = await response.json();
+
+            if (!response.ok) {
+              // MFA verification failed
+              alert(`Error: ${result.message}`);
+            } else {
+              // Signin w/ MFA successful
+              mfaDialog.open = false;
+              sessionStorage.setItem('username', result.username);
+              const username = sessionStorage.getItem('username');
+
+              updateUserUI(username, result.visitorId);
+              alert(result.message);
+            }
+          });
+        }
+        // Regardless of MFA, show the error message
+        alert(`Error: ${result.message}`);
+      } else {
+        // Signin successful
+        sessionStorage.setItem('username', result.username);
+        const username = sessionStorage.getItem('username');
+
+        updateUserUI(username, result.visitorId);
+        alert(result.message);
+      }
+    });
+
+    registerForm.addEventListener('submit', async (event) => {
+      // Register form submission
+      event.preventDefault();
+      registerDialog.open = false;
+
+      const email = registerForm.querySelector("wa-input[label='Email']").value;
+      const password = registerForm.querySelector("wa-input[label='Password']").value;
+      const results = await fp.get({ extendedResult: true });
+      const visitorId = results.visitorId;
+
+      // prod: "https://prototype-backend-hw7a.onrender.com/register"
+      // dev: "http://localhost:3001/register"
+      const response = await fetch('http://localhost:3001/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password, visitorId }),
+      });
+
+      const result = await response.json();
+
+      if (!response.ok) {
+        // Registration failed
+        alert(`Error: ${result.message}`);
+      } else {
+        // Registration successful
+        sessionStorage.setItem('username', result.username);
+        const username = sessionStorage.getItem('username');
+        const visitorId = result.visitorId;
+
+        updateUserUI(username, visitorId);
+        alert(result.message);
+      }
+    });
+
+    const updateUserUI = (username, visitorId) => {
+      document.querySelector('.wa-cluster.wa-gap-xs').style.display = 'none';
+
+      const logoutInfo = document.getElementById('logout-info');
+      const tooltip = document.getElementById('tooltip-display');
+      const tooltipText = document.getElementById('tooltip-text');
+      const usernameHeading = document.querySelector('.username.wa-heading-s');
+      const usernameCaption = document.querySelector('.username.wa-caption-s');
+      const userAvatar = document.querySelector('.user-avatar');
+
+      usernameHeading.textContent = `Hi ${username}!`;
+      usernameCaption.textContent = 'View Cart';
+      tooltip.style.display = 'initial';
+      tooltipText.innerHTML = `Known Visitor IDs:<br>${visitorId}`;
+      userAvatar.setAttribute('image', './assets/avatar.jpg');
+      logoutInfo.style.display = 'initial';
+    };
+  });
+
+  // ----- Logout functionality -----
+
+  const logoutBtn = document.getElementById('logout-btn');
+
+  logoutBtn.addEventListener('click', () => {
+    sessionStorage.removeItem('username');
+    window.location.reload();
+  });
 })();
--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
-import { FingerprintJsServerApiClient, Region, isValidWebhookSignature } from "@fingerprintjs/fingerprintjs-pro-server-api";
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import {
+  FingerprintJsServerApiClient,
+  Region,
+  isValidWebhookSignature,
+} from '@fingerprintjs/fingerprintjs-pro-server-api';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,19 +15,23 @@ dotenv.config();
 app.use(express.json());
 
 const allowedOrigins = [
-  "https://prototype-client.onrender.com",
-  "https://projectshowcase.dev",
-  "projectshowcase.dev",
-  "http://127.0.0.1:5500"
+  'https://prototype-client.onrender.com',
+  'https://projectshowcase.dev',
+  'projectshowcase.dev',
+  'http://127.0.0.1:5500',
 ];
+
+const users = [];
 
 app.use(
   cors({
     origin: (origin, callback) => {
-      allowedOrigins.includes(origin) || !origin ? callback(null, true) : callback(new Error("Not allowed by CORS"));
+      allowedOrigins.includes(origin) || !origin
+        ? callback(null, true)
+        : callback(new Error('Not allowed by CORS'));
     },
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
   })
 );
 
@@ -31,15 +40,15 @@ const client = new FingerprintJsServerApiClient({
   region: Region.Global,
 });
 
-app.get("/", (req, res) => {
-  res.status(200).send("Well, hello there!");
+app.get('/', (req, res) => {
+  res.status(200).send('Well, hello there!');
 });
 
-app.get("/health", (req, res) => {
-  res.status(200).send("Healthy!");
+app.get('/health', (req, res) => {
+  res.status(200).send('Healthy!');
 });
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   const requestId = req.body.requestId;
 
   client.getEvent(requestId).then((data) => {
@@ -48,32 +57,127 @@ app.post("/", (req, res) => {
   });
 });
 
-app.post("/webhook", (req, res) => {
+app.post('/signin', async (req, res) => {
+  const { email, password, visitorId } = req.body;
+  const user = users.find((user) => user.email === email);
+
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+
+  const visitorIdExists = users.some((user) => user.visitorIds.includes(visitorId));
+
+  if (!visitorIdExists) {
+    return res.status(403).json({
+      error: 'verification_required',
+      message: 'MFA needed before completing signin',
+    });
+  }
+
+  return completeSignIn(user, email, res);
+});
+
+app.post('/register', async (req, res) => {
+  const { email, password, visitorId } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email & Password are required!' });
+  }
+
+  if (!visitorId) {
+    return res.status(400).json({ message: 'Visitor ID is required!' });
+  }
+
+  const existingUser = users.find((user) => user.email === email);
+  if (existingUser) {
+    return res.status(400).json({ message: 'User already exists!' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  users.push({
+    email: email,
+    password: hashedPassword,
+    visitorIds: [visitorId],
+  });
+
+  const username = email.split('@')[0];
+  const capitalizedUsername = username.charAt(0).toUpperCase() + username.slice(1);
+
+  res.status(200).json({
+    message: 'Successfully registered!',
+    username: capitalizedUsername,
+    visitorId: visitorId,
+  });
+
+  console.log('from /register', users);
+});
+
+app.post('/verify', async (req, res) => {
+  const { email, visitorId, mfaCode } = req.body;
+  const user = users.find((user) => user.email === email);
+  console.log(mfaCode);
+
+  // Simulate MFA verification
+  if (mfaCode != 123456) {
+    return res.status(401).json({ message: 'Invalid MFA code' });
+  }
+
+  if (!user.visitorIds.includes(visitorId)) {
+    user.visitorIds.push(visitorId);
+  }
+
+  return completeSignIn(user, email, res);
+});
+
+app.post('/webhook', (req, res) => {
   (async () => {
     try {
-      const secret = process.env.WEBHOOK_SIGNATURE_SECRET
-      const header = req.headers["fpjs-event-signature"];
+      const secret = process.env.WEBHOOK_SIGNATURE_SECRET;
+      const header = req.headers['fpjs-event-signature'];
       const data = JSON.stringify(req.body);
-  
+
       if (!secret) {
         return res.status(500).json('Secret is not set.');
       }
-  
+
       if (!header) {
         return res.status(400).json('fpjs-event-signature header not found.');
       }
-  
+
       if (!isValidWebhookSignature({ header, data, secret })) {
         return res.status(403).json('Webhook signature is invalid.');
       }
-  
+
       const visitorId = req.body.visitorId;
-      return res.status(200).json(`Webhook received! Here's your visitor ID: ${(visitorId)}`);
+      return res.status(200).json(`Webhook received! Here's your visitor ID: ${visitorId}`);
     } catch (error) {
       return res.status(500).json(`Error: ${error}`);
     }
   })();
 });
+
+async function completeSignIn(user, email, res) {
+  const username = user.email.split('@')[0];
+  const capitalizedUsername = username.charAt(0).toUpperCase() + username.slice(1);
+
+  const visitorIds = users
+    .filter((user) => user.email === email)
+    .map((user) => user.visitorIds)
+    .flat()
+    .join(',\n');
+
+  return res.status(200).json({
+    message: 'Successfully signed in!',
+    username: capitalizedUsername,
+    visitorId: visitorIds,
+  });
+}
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
